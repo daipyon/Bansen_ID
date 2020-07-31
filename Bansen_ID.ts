84ef6c5d2751cc117e8066b74ca3ae0fc42f7ddd
daipyon <52702633+daipyon@users.noreply.github.com>
2020-07-31 14:24:21 +0900

    Update Bansen_ID.ts

--- Bansen_ID.ts
+++ Bansen_ID.ts
@@ -5,8 +5,10 @@ import { rootCertificates } from 'tls';
 var fs = require('fs');
 
 // 読み込みする スプレッドシートID と シート名 の記載
-const SSID = process.env.SheetID;
-const SSName1 = process.env.SheetName_1;
+// const SSID = process.env.SheetID;
+// const SSName1 = process.env.SheetName_1;
+const SSID = process.env.BANSEN_SheetID;
+const SSName1 = process.env.BANSEN_SheetName;
 // 画像などを保存するフォルダのパスを記載する。
 const DownloadFolder = __dirname + '/Download/';
 // Abematvのログイン。 メールアドレス・パスワードの記載 <<漏洩注意>>
